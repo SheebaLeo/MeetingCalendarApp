c5554e54a42767d43e6a6158c1e5252a75e93695
Phiomin Gnana Sheeba <jsheeba1893@gmail.com>
2024-12-05 12:29:31 +0100

    Initial commit with backend and frontend

---
+++ backend/MeetingCalendarAppBackend/src/main/java/com/example/meetingcalendarappbackend/MeetingCalendarAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetingcalendarappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingCalendarAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingCalendarAppBackendApplication.class, args);
+    }
+
+}
---
+++ backend/MeetingCalendarAppBackend/src/test/java/com/example/meetingcalendarappbackend/MeetingCalendarAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetingcalendarappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingCalendarAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
